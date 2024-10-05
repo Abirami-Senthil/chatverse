4ad63575ab051172694a3231ca7f881bf3286ace
Abirami S <abirami.s77@gmail.com>
2024-10-05 23:46:01 +0530

    modify(ui): changed avatars and input section and buttons in the chat panel

--- chatbot-app/src/components/ChatWindow.tsx
+++ chatbot-app/src/components/ChatWindow.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { FiX, FiEdit2, FiTrash2 } from 'react-icons/fi';
+import { FiX, FiEdit2, FiTrash2, FiCheck, FiSend } from 'react-icons/fi';
 import { ChatController } from '../controllers/ChatController';
+import './ChatWindow.css';
 
 interface ChatWindowProps {
     toggleChat: () => void;
@@ -85,7 +86,6 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ toggleChat }) => {
         console.log(updatedInteraction)
         if (updatedInteraction) {
             setMessages((prevMessages) => {
-                debugger;
                 const updatedMessages = [...prevMessages];
                 updatedMessages[isEditing.index!] = { sender: 'user', text: isEditing.text, interactionId: isEditing.interactionId };
                 updatedMessages[isEditing.index! + 1] = { sender: 'bot', text: updatedInteraction.interaction.response, interactionId: updatedInteraction.interaction.interaction_id };
@@ -101,36 +101,35 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ toggleChat }) => {
     const handleDeleteClick = async (interactionId: string) => {
         const remainingInteractions = await chatController.deleteMessage(interactionId);
         if (remainingInteractions) {
-          setMessages(remainingInteractions.remaining_interactions.flatMap((interaction: any) => {
-            const expandedMessages = [];
-            if (interaction.message) {
-              expandedMessages.push({
-                sender: 'user',
-                text: interaction.message,
-                interactionId: interaction.interaction_id,
-              });
-            }
-            expandedMessages.push({
-              sender: 'bot',
-              text: interaction.response,
-              interactionId: interaction.interaction_id,
-            });
-            return expandedMessages;
-          }));
+            setMessages(remainingInteractions.remaining_interactions.flatMap((interaction: any) => {
+                const expandedMessages = [];
+                if (interaction.message) {
+                    expandedMessages.push({
+                        sender: 'user',
+                        text: interaction.message,
+                        interactionId: interaction.interaction_id,
+                    });
+                }
+                expandedMessages.push({
+                    sender: 'bot',
+                    text: interaction.response,
+                    interactionId: interaction.interaction_id,
+                });
+                return expandedMessages;
+            }));
         }
-      };
-      
+    };
 
     return (
         <div className="w-96 h-[600px] bg-white rounded-lg shadow-2xl fixed bottom-20 right-5 flex flex-col">
             <div className="flex flex-col items-center justify-center p-4 bg-white rounded-t-lg">
                 <img
-                    src="https://via.placeholder.com/40"
+                    src="/images/abi.jpg"
                     alt="avatar"
                     className="w-10 h-10 rounded-full mb-2"
                 />
                 <div className="text-center">
-                    <h4 className="text-sm font-bold">Hey 👋, I'm Ava</h4>
+                    <h4 className="text-sm font-bold">Hey 👋, I'm Abi</h4>
                     <p className="text-xs text-gray-500">Ask me anything or pick a place to start</p>
                 </div>
                 <button className="text-gray-500 absolute top-2 right-2" onClick={toggleChat}>
@@ -139,8 +138,15 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ toggleChat }) => {
             </div>
             <div className="flex-1 p-4 overflow-y-auto">
                 {messages.map((message, index) => (
-                    <div key={index} className={`mb-2 ${message.sender === 'user' ? 'text-right text-white' : 'text-left text-black'}`}>
-                        <div className={`inline-block px-4 py-3 rounded-lg relative ${message.sender === 'user' ? 'max-w-[75%] text-left' : 'max-w-full'} break-words ${message.sender === 'user' ? 'bg-purple-600' : 'bg-gray-50'}`}>
+                    <div key={index} className={`mb-2 flex items-start ${message.sender === 'user' ? 'justify-end' : 'justify-start'}`}>
+                        {message.sender === 'bot' && (
+                            <img
+                                src="/images/abi.jpg"
+                                alt="avatar"
+                                className="w-10 h-10 rounded-full mr-2"
+                            />
+                        )}
+                        <div className={`inline-block px-4 py-3 relative ${message.sender === 'user' ? 'max-w-[75%] text-left' : 'max-w-full'} break-words ${message.sender === 'user' ? 'bg-custom-purple text-white rounded-b-3xl rounded-tl-3xl' : 'bg-gray-50'}`}>
                             {isEditing.index === index ? (
                                 <textarea
                                     value={isEditing.text}
@@ -152,7 +158,7 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ toggleChat }) => {
                                 <p className="pr-8 break-words text-sm">{message.text}</p>
                             )}
                             {message.sender === 'user' && message.interactionId && (
-                                <div className="absolute top-0 right-0 mt-1 flex space-x-0.5 z-10">
+                                <div className="absolute top-0 right-1 mt-1 flex space-x-0.5 z-10">
                                     <button onClick={() => handleEditClick(index, message.interactionId)} className="text-black hover:text-gray-800 rounded-full px-0.5">
                                         <FiEdit2 size={14} />
                                     </button>
@@ -166,8 +172,14 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ toggleChat }) => {
                 ))}
                 <div ref={messagesEndRef} />
             </div>
-            <div className="p-4 border-t border-gray-200">
-                <div className="relative">
+            <div className="border-t border-gray-200 mx-4"></div>
+            <div className="p-4">
+                <div className="relative flex items-center">
+                    <img
+                        src="/images/userIcon.jpg"
+                        alt="avatar"
+                        className="w-6 h-6 rounded-full mr-2"
+                    />
                     <textarea
                         id="chat-input-field"
                         value={isEditing.index !== null ? isEditing.text : input}
@@ -175,40 +187,42 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ toggleChat }) => {
                         placeholder="Your question"
                         onKeyDown={(e) => {
                             if (e.key === 'Enter' && !e.shiftKey) {
-                              e.preventDefault();
-                              if (isEditing.index !== null) {
-                                handleEditSave();
-                              } else {
-                                sendMessage();
-                              }
+                                e.preventDefault();
+                                if (isEditing.index !== null) {
+                                    handleEditSave();
+                                } else {
+                                    sendMessage();
+                                }
                             }
-                          }}
-                        className="w-full px-3 py-2 pr-14 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+                        }}
+                        className="w-full px-3 py-2 pr-14 border border-transparent rounded-md focus:outline-none resize-none overflow-auto"
                         rows={2}
                     />
-                    {isEditing.index !== null && (
-                        <button
-                            onClick={() => setIsEditing({ index: null, text: '', interactionId: '' })}
-                            className="absolute right-2 top-1/2 transform -translate-y-1/2 text-blue-600 text-xs"
-                        >
-                            Cancel
-                        </button>
-                    )}
                 </div>
                 {isEditing.index !== null ? (
-                    <button
-                        onClick={handleEditSave}
-                        className="mt-2 w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
-                    >
-                        Save
-                    </button>
+                    <div className="flex justify-end mt-2 space-x-2 pr-2 pb-2">
+                        <button
+                            onClick={() => setIsEditing({ index: null, text: '', interactionId: '' })}
+                            className="text-red-600 text-xs"
+                        >
+                            <FiX size={24} />
+                        </button>
+                        <button
+                            onClick={handleEditSave}
+                            className="text-green-600 text-xs"
+                        >
+                            <FiCheck size={24} />
+                        </button>
+                    </div>
                 ) : (
-                    <button
-                        onClick={sendMessage}
-                        className="mt-2 w-full bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
-                    >
-                        Send
-                    </button>
+                    <div className="flex justify-end mt-2 pr-2 pb-2">
+                        <button
+                            onClick={sendMessage}
+                            className="text-gray-500 text-xs rotate-45"
+                        >
+                            <FiSend size={24} />
+                        </button>
+                    </div>
                 )}
             </div>
         </div>
